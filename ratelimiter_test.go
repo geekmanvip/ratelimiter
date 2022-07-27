3e76403ce8014ad798a1e22100322799ff8e5cfc
yuanliang <yuanliang@corp-ci.com>
2022-07-27 16:21:51 +0800

    增加测试用例

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -10,35 +10,59 @@ import (
 )
 
 func TestSetRedisStorage(t *testing.T) {
-	ratelimiter.SetRedisStorage(ratelimiter.RedisConfig{
+	err := ratelimiter.SetRedisStorage(ratelimiter.RedisConfig{
 		Host:     "127.0.0.1",
 		Port:     6379,
 		Password: "",
 		Db:       0,
 	})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
+// 固定时间窗口测试
 func TestWindowLimiter(t *testing.T) {
-	//limiter := ratelimiter.NewWindowLimiter(1000, 5).WithRedis("test")
-	//test(limiter)
+	limiter := ratelimiter.NewWindowLimiter(1000, 5)
+	test(limiter)
+}
+func TestWindowLimiter_WithRedis(t *testing.T) {
+	limiter := ratelimiter.NewWindowLimiter(1000, 5).WithRedis("test")
+	test(limiter)
 }
 
+// 滑动时间窗口测试
 func TestSlideWindowLimiter(t *testing.T) {
-	//limiter := ratelimiter.NewSlideWindowLimiter(1000, 1)
-	//test(limiter)
+	limiter := ratelimiter.NewSlideWindowLimiter(1000, 1)
+	test(limiter)
+}
+func TestSlideWindowLimiter_WithRedis(t *testing.T) {
+	limiter := ratelimiter.NewSlideWindowLimiter(1000, 1).WithRedis("test")
+	test(limiter)
 }
 
+// 漏桶测试
 func TestLeakBucketLimiter(t *testing.T) {
-	//limiter := ratelimiter.NewLeakBucketLimiter(4, 2)
-	//test(limiter)
+	limiter := ratelimiter.NewLeakBucketLimiter(4, 2)
+	test(limiter)
+}
+func TestLeakBucketLimiter_WithRedis(t *testing.T) {
+	limiter := ratelimiter.NewLeakBucketLimiter(4, 2).WithRedis("test")
+	test(limiter)
 }
 
+// 令牌桶测试
 func TestTokenBucketLimiter(t *testing.T) {
 	limiter := ratelimiter.NewTokenBucketLimiter(4, 2)
 	test(limiter)
 }
+func TestTokenBucketLimiter_WithRedis(t *testing.T) {
+	limiter := ratelimiter.NewTokenBucketLimiter(4, 2).WithRedis("test")
+	test(limiter)
+}
 
 func test(limiter ratelimiter.Limiter) {
+	fmt.Printf("\n%T\n", limiter)
 	if err := limiter.Err(); err != nil {
 		log.Println(err)
 		return
